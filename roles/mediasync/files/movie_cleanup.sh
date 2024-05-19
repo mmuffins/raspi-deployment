0136512aeb79855f16bcf7ee29a9efc6e1ee1f83
Michael Kellner <mail@tiv73.com>
2024-05-19 10:46:12 +0200

    Improve media sync share handling (#10)
    * Improve mediasync
    
    * Forcefully kill rsync if mount cannot be umounted

--- roles/mediasync/files/movie_cleanup.sh
+++ roles/mediasync/files/movie_cleanup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 source ./variables
 
-if ! (mount | grep //brownie.lan/Maxtor/NVIDIA_SHIELD > /dev/null); then
+if ! (mount | grep "$smb_share" > /dev/null); then
 	echo "Mounting $smb_share to $local_mountpoint"
-	mkdir $local_mountpoint
+	mkdir -p $local_mountpoint
 	sudo mount -t cifs $smb_share $local_mountpoint -o vers=3.0,iocharset=utf8,username=$smb_username,password=$smb_password
+fi
+
+if ! (mount | grep "$smb_share" > /dev/null); then
+  echo "Could not mount $smb_share, aborting script"
+  # Exit with 0 to indicate success to not prevent running other scripts
+  exit 0
 fi
 
 for d in "$remote_movies_dir/"*/ ; do
--- roles/mediasync/files/unmount_share.sh
+++ roles/mediasync/files/unmount_share.sh
@@ -2,5 +2,27 @@
 source ./variables
 
 if mount | grep "$smb_share" > /dev/null; then
-	umount /mnt/remotes/brownie
+  remote="/mnt/remotes/brownie"
+  # Run multiple times to kill strugglers
+  for i in {1..3}
+  do
+    rsync_processes=$(ps aux | grep -i "rsync" | grep "$remote")
+    if [ -z "$rsync_processes" ]; then
+      echo "No rsync processes found copying to $remote (Attempt $i), unmounting share"
+      umount "$remote"
+      exit 0
+    else
+      echo "$rsync_processes" | awk '{print $2}' | xargs -r kill
+      echo "Killed all rsync processes copying to $remote (Attempt $i)"
+    fi
+
+    if [ $i -lt 3 ]; then
+      sleep 30
+    fi
+  done
+
+  echo "Final attempt to unmount $remote"
+  umount "$remote"
 fi
+
+
